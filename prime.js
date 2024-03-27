e55fc1a8aa12292b8d9b819088266284bb32fcc2
“Shashank_B” <shashank.b@codecraft.co.in>
2024-03-27 15:42:06 +0530

    Assertion implemented for prime function

---
+++ prime.js
@@ -0,0 +1,36 @@
+function assert(condition,message){
+    if(condition){
+        throw new Error(message);
+        // console.error(message);
+    }
+}
+
+
+function prime(a){
+    //pre-contions
+    //must be a number
+    //must be greater than 1
+    //must not be prime
+    let isNotPrime=false;
+    assert(typeof a!=='number',"a non-number parameter is detected");
+    assert(a<2,"parameter must be greater that or equal to 2");
+
+    for(let i=2;i<=Math.sqrt(a);i++){
+        if(a%i===0){
+            isNotPrime=true;
+            assert(isNotPrime,`${a} is not prime number`);
+            break;
+        }
+    }
+
+    
+}
+
+//post-conditions
+prime(2);
+prime(1);
+prime();
+prime("Hello");
+prime(93);
+prime(10);
+
