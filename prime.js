73855f7b375f5fb00e4a820d677a2f39fabeb627
Shashank <shashank.b@codecraft.co.in>
2024-03-28 15:50:01 +0530

    Implmented generatePrimeSeries function and added assertions for both pre and post conditions

--- prime.js
+++ prime.js
@@ -1,40 +1,87 @@
-function assert(condition,message){
-    if(condition){
-        throw new Error(message);
-        // console.error(message);
+import { input } from "@inquirer/prompts";
+
+import assert from "assert";
+
+function generatePrimeSeries(count) {
+  //pre-condition
+  //must be a number
+  if (typeof count !== "number") {
+    return false;
+  }
+  //must be a positive integer or greater that or equal to 1
+  if (count < 1) {
+    return [];
+  }
+
+  const primeNumbers = [];
+  let firstNum = 2;
+  while (primeNumbers.length < count) {
+    if (isPrime(firstNum)) {
+      primeNumbers.push(firstNum);
     }
+    firstNum++;
+  }
+  return primeNumbers;
 }
 
-
-function prime(a){
-    //pre-contions
-    //must be a number
-    //must be greater than 1
-    //must not be prime
-    let isNotPrime=false;
-    assert(typeof a!=='number',"a non-number parameter is detected");
-    assert(a<2,"parameter must be greater that or equal to 2");
-
-    for(let i=2;i<=Math.sqrt(a);i++){
-        if(a%i===0){
-            isNotPrime=true;
-            assert(isNotPrime,`${a} is not prime number`);
-            break;
-        }
-        // assert(a%i===0,`${a} is not prime number`);
+function isPrime(num) {
+  // pre conditions
+  //its must always a number
+  if (typeof num !== "number") {
+    return false;
+  }
+  //  primes are always > 1, and they will have a factor other than themselves or 1
+  if (num < 1) {
+    return false;
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
     }
-   if(!isNotPrime && typeof a==='number' && a>1){
-    console.log(`${a} is a prime number`)
-   }    
-    
+  }
+  return true;
 }
 
-//post-conditions
-prime(2);
-prime(7);
-prime(1);
-prime();
-prime("Hello");
-prime(93);
-prime(10);
+//Post-conditions for isPrime function
 
+//Test cases (postive)
+assert(isPrime(2), "2 is a prime number");
+assert(isPrime(4), "4 is not a prime number");
+assert(isPrime(5), "5 is a prime number");
+assert(isPrime(6), "6 is not a prime number");
+assert(isPrime(29), "29 is  a prime number");
+
+//Test cases (negativr)
+assert(isPrime(-2), "-2 is not a prime number");
+assert(isPrime(0), "0 is not a prime number");
+assert(isPrime(-7), "-7 is not a prime number");
+
+//Test cases (non-numbers)
+assert(isPrime("a"), "a is not a number");
+assert(isPrime(), "Undefined is not an integer");
+assert(isPrime("*"), "* is not an integer");
+
+//Post-conditions for generatePrimeSeries
+
+assert.deepStrictEqual(
+  generatePrimeSeries(5),
+  [2, 3, 5, 7, 11],
+  "Count 5 must return [2,3,5,7,11]"
+);
+assert.deepStrictEqual(
+  generatePrimeSeries(0),
+  [],
+  "It should be an empty array"
+);
+assert.deepStrictEqual(generatePrimeSeries(1), [2], "Count 2 must return [2]");
+assert.deepStrictEqual(
+  generatePrimeSeries(3),
+  [2, 3, 5],
+  "Count 5 must return [2,3,5]"
+);
+// assert.deepStrictEqual(generatePrimeSeries("a"), [], "Input must be a number");
+assert.deepStrictEqual(
+  generatePrimeSeries(-3),
+  [],
+  "Input must be greater than or equal to 1"
+);
