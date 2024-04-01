30d9d567ec3770c672fa0387ab0e82160f55f9fd
“Shashank_B” <shashank.b@codecraft.co.in>
2024-04-01 14:57:54 +0530

    Added assert.throws to all negative examples for both prime and generatePrime functions

--- prime.js
+++ prime.js
@@ -4,14 +4,8 @@ import assert from "assert";
 
 function generatePrimeSeries(count) {
   //pre-condition
-  //must be a number
-  if (typeof count !== "number") {
-    return false;
-  }
-  //must be a positive integer or greater that or equal to 1
-  if (count < 1) {
-    return [];
-  }
+  assert(typeof count === "number", "Argument should be a number");
+  assert(count > 0, "Argument should be greater than or equal to 1");
 
   const primeNumbers = [];
   let firstNum = 2;
@@ -27,15 +21,15 @@ function generatePrimeSeries(count) {
 function isPrime(num) {
   // pre conditions
   //its must always a number
-  if (typeof num !== "number") {
-    return false;
-  }
+  assert(typeof num === "number", "Argument should be a number");
   //  primes are always > 1, and they will have a factor other than themselves or 1
-  if (num < 1) {
-    return false;
-  }
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
+  assert(num > 1, "Argument should be greater than 1");
+
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
       return false;
     }
   }
@@ -46,20 +40,30 @@ function isPrime(num) {
 
 //Test cases (postive)
 assert(isPrime(2), "2 is a prime number");
-assert(isPrime(4), "4 is not a prime number");
 assert(isPrime(5), "5 is a prime number");
-assert(isPrime(6), "6 is not a prime number");
 assert(isPrime(29), "29 is  a prime number");
+assert(isPrime(999999000001), "999999000001 is a prime number");
+assert(!isPrime(4), "4 is not a prime number");
+assert(!isPrime(6), "6 is not a prime number");
+assert(isPrime(67280421310721), "67280421310721 is not a prime number");
 
-//Test cases (negativr)
-assert(isPrime(-2), "-2 is not a prime number");
-assert(isPrime(0), "0 is not a prime number");
-assert(isPrime(-7), "-7 is not a prime number");
+//Test cases (negative)
 
-//Test cases (non-numbers)
-assert(isPrime("a"), "a is not a number");
-assert(isPrime(), "Undefined is not an integer");
-assert(isPrime("*"), "* is not an integer");
+assert.throws(
+  () => !isPrime(-2),
+  /Argument should be greater than 1/,
+  "Negative numbers cannot be a prime numbers"
+);
+assert.throws(
+  () => !isPrime(0),
+  /Argument should be greater than 1/,
+  "Negative numbers cannot be a prime numbers"
+);
+assert.throws(
+  () => !isPrime("a"),
+  /Argument should be a number/,
+  "The type should be a number"
+);
 
 //Post-conditions for generatePrimeSeries
 
@@ -68,20 +72,25 @@ assert.deepStrictEqual(
   [2, 3, 5, 7, 11],
   "Count 5 must return [2,3,5,7,11]"
 );
-assert.deepStrictEqual(
-  generatePrimeSeries(0),
-  [],
-  "It should be an empty array"
-);
-assert.deepStrictEqual(generatePrimeSeries(1), [2], "Count 2 must return [2]");
+assert.deepStrictEqual(generatePrimeSeries(1), [2], "Count 1 must return [2]");
 assert.deepStrictEqual(
   generatePrimeSeries(3),
   [2, 3, 5],
-  "Count 5 must return [2,3,5]"
+  "Count 3 must return [2,3,5]"
 );
-// assert.deepStrictEqual(generatePrimeSeries("a"), [], "Input must be a number");
-assert.deepStrictEqual(
-  generatePrimeSeries(-3),
-  [],
-  "Input must be greater than or equal to 1"
+
+assert.throws(
+  () => generatePrimeSeries(-1),
+  /Argument should be greater than or equal to 1/,
+  "The value must be greater than 0"
+);
+assert.throws(
+  () => generatePrimeSeries("a"),
+  /Argument should be a number/,
+  "The value must be a number"
+);
+assert.throws(
+  () => generatePrimeSeries([]),
+  /Argument should be a number/,
+  "The value must be a number"
 );
